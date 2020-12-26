c03567df469b8253f3dce0abb3ce0ccc91201b46
Karina <karina.shokur@devit-team.com>
2020-12-26 04:58:37 +0000

    Initial Commit with database created

--- database/migrations/2018_07_16_000005_create_billings_table.php
+++ database/migrations/2018_07_16_000005_create_billings_table.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-class CreateFacturationsTable extends Migration
+class CreateBillingsTable extends Migration
 {
     public function up()
     {

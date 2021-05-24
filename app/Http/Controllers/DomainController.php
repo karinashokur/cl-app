4f6020fa96887eab5115569468bb4dfa8d19d86f
Karina <karina.shokur@devit-team.com>
2021-05-24 00:59:38 +0000

    Domain controller

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -6,23 +6,30 @@ class DomainController extends Controller
 {
     public function index()
     {
-    }
-    public function create()
-    {
+      $domains = Domain::all();
+      return response()->json($domains, 200);
     }
     public function store(Request $request)
     {
+      Domain::create($request->all());
+      return response()->json('Domain was successfuly stored.', 200);
     }
     public function show(Domain $domain)
     {
+        return response()->json($domain, 200);
     }
     public function edit(Domain $domain)
     {
+        return response()->json($domain, 200);
     }
     public function update(Request $request, Domain $domain)
     {
+      $domain->update($request->all());
+      return response()->json('Domain was successfuly updated.', 200);
     }
     public function destroy(Domain $domain)
     {
+      $domain->delete();
+      return response()->json('Domain was successfuly deleted', 200);
     }
 }
